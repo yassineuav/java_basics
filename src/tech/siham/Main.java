5684d4e00765ee8e5ad6b35bdfd93d6f905d1aa2
Yassine <yassineuav@gmail.com>
2023-01-29 12:36:15 +0100

    java basics

---
+++ src/tech/siham/Main.java
@@ -0,0 +1,13 @@
+package tech.siham;
+
+import java.util.Arrays;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int[] numbers = {1, 2, 3, 4, 5};
+        
+        System.out.println(Arrays.toString(numbers));
+        
+    }
+}
